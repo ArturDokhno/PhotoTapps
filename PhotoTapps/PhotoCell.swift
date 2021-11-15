c57b1ca78863ee60d9e493244a9a58f3d5904006
ArturDokhno <artur.dohno@icloud.com>
2021-11-15 20:44:56 +0500

    Работа с PhotoCell и Cell

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Артур Дохно on 15.11.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
--- PhotoTapps/PhotoCollectionVC.swift
+++ PhotoTapps/PhotoCollectionVC.swift
@@ -7,34 +7,32 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class PhotoCollectionVC: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 0
-        
+        return 1
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
-        
+        return 15
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
     
+        cell.backgroundColor = .black
     
         return cell
     }
 
+    
+}
+
+extension PhotoCollectionVC: UICollectionViewDelegateFlowLayout {
     
 }
